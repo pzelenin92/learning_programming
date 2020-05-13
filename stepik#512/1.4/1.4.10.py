b84b45f80f380eff0a44e7e8487760367b835105
pzelenin92 <pzelenin92@gmail.com>
2020-05-13 19:35:15 +0300

    adden some functions

--- stepik#512/1.4/1.4.10.py
+++ stepik#512/1.4/1.4.10.py
@@ -16,46 +16,145 @@ def createquery(y,z):#create <namespace> <parent> –  создать новое
     namespace=[]
     namespace.append(y)
     namespaces[z]=namespace
-def getquery(y,z):#get <namespace> <var> – получить имя пространства, из которого будет взята переменная <var> при запросе из пространства <namespace>, или None, если такого пространства не существует
-
+def gettquery(y,z):#get <namespace> <var> – получить имя пространства, из которого будет взята переменная <var> при запросе из пространства <namespace>, или None, если такого пространства не существует
+    result=None
+    def getquery(y,z):
+        nonlocal result
+        if z in namespaces[y]:
+            result=y
+        else:
+            for i in namespaces[y]:
+                if type(i) is dict:
+                    y=i['parent']
+                    getquery(y,z)
+    getquery(y,z)
+    return result
 #main
 n=int(input())
 namespaces=dict()
-i=0
-while i<n:
+#i=0
+for i in range(n):
     x,y,z=input().split()
     if x=='add':
         addquery(y,z)
-        i+=1
+    elif x=='create':
+        createquery(y,z)
     else:
-        i+=1
-        continue
-print(namespaces)
+        print(gettquery(y,z))
 
-'''namespaces={'global':['a','b','None'],'bar':['c','d',{'parent':'global'}],'foo':['e','f',{'parent':'bar'}]}
+#functions
+def addquery(y,z):# add <namespace> <var> – добавить в пространство <namespace> переменную <var>
+    if y in namespaces:
+        namespace=namespaces[y]
+        namespace.append(z)
+    else:
+        namespace=[]
+        namespace.append(z)
+        namespaces[y]=namespace
+def createquery(y,z):#create <namespace> <parent> –  создать новое пространство имен с именем <namespace> внутри пространства <parent>
+    namespace=[]
+    namespace.append(y)
+    namespaces[z]=namespace
+def gettquery(y,z):#get <namespace> <var> – получить имя пространства, из которого будет взята переменная <var> при запросе из пространства <namespace>, или None, если такого пространства не существует
+    result=None
+    def getquery(y,z):
+        nonlocal result
+        if z in namespaces[y]:
+            result=y
+        else:
+            for i in namespaces[y]:
+                if type(i) is dict:
+                    y=i['parent']
+                    getquery(y,z)
+    getquery(y,z)
+    return result
+#main
+data='add global a\n'\
+'create foo global\n'\
+'add foo b\n'\
+'get foo a\n'\
+'get foo c\n'\
+'create bar foo\n'\
+'add bar a\n'\
+'get bar a\n'\
+'get bar b'
+xdata=data.splitlines()
+namespaces=dict()
+#i=0
+for i in xdata:
+    x,y,z=i.split()
+    if x=='add':
+        addquery(y,z)
+    elif x=='create':
+        createquery(y,z)
+    else:
+        print(gettquery(y,z))
 
-if 'a' in namespaces['foo']:
-    print('yes')
-elif 
-    if 'a' in namespaces['bar']:
-        print('pidor')
+"""def createquery(y,z):#create <namespace> <parent> –  создать новое пространство имен с именем <namespace> внутри пространства <parent>
+    if :
 
-def getquery(y,z):
-    if z not in namespaces[y]:'''
+        
 
-'''a='add global a'
-b='add global b'
-c='add bar a'''''
-"""x,y,z=c.split()
-addquery(y,z)
-print(namespaces)"""
-'''value=[]
-x,y,z=a.split()
-if x=='add':
-    value.append(z)
-    namespaces[y]=value
-value=[]
-x,y,z=b.split()
-if x=='add':
-    value.append(z)
-    namespaces[y]=value'''
+data='add global a\n'\
+'create foo global\n'\
+'add foo b\n'\
+'get foo a\n'\
+'get foo c\n'\
+'create bar foo\n'\
+'add bar a\n'\
+'get bar a\n'\
+'get bar b'
+xdata=data.splitlines()
+namespaces={}
+for i in xdata:
+    x,y,z=i.split()
+    if x=='create':
+        createquery(y,z)
+"""
+"""#functions
+def addquery(y,z):# add <namespace> <var> – добавить в пространство <namespace> переменную <var>
+    if y in namespaces:
+        namespace=namespaces[y]
+        namespace.append(z)
+    else:
+        namespace=[]
+        namespace.append(z)
+        namespaces[y]=namespace
+def createquery(y,z):#create <namespace> <parent> –  создать новое пространство имен с именем <namespace> внутри пространства <parent>
+    namespace=[]
+    namespace.append(y)
+    namespaces[z]=namespace
+def gettquery(y,z):#get <namespace> <var> – получить имя пространства, из которого будет взята переменная <var> при запросе из пространства <namespace>, или None, если такого пространства не существует
+    result=None
+    def getquery(y,z):
+        nonlocal result
+        if z in namespaces[y]:
+            result=y
+        else:
+            for i in namespaces[y]:
+                if type(i) is dict:
+                    y=i['parent']
+                    getquery(y,z)
+    getquery(y,z)
+    return result
+#main
+data='add global a\n'\
+'create foo global\n'\
+'add foo b\n'\
+'get foo a\n'\
+'get foo c\n'\
+'create bar foo\n'\
+'add bar a\n'\
+'get bar a\n'\
+'get bar b'
+xdata=data.splitlines()
+namespaces=dict()
+#i=0
+for i in xdata:
+    x,y,z=i.split()
+    if x=='add':
+        addquery(y,z)
+    elif x=='create':
+        createquery(y,z)
+    else:
+        print(gettquery(y,z))"""
