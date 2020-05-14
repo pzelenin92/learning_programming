6cd595ef3022e356b64c41115c404f0a971a7ce0
pzelenin92 <60407359+pzelenin92@users.noreply.github.com>
2020-05-15 00:17:44 +0300

    Working version

--- stepik#512/1.4/1.4.10/1.4.10.1_createquery_findcurrentspace.py
+++ stepik#512/1.4/1.4.10/1.4.10.1_createquery_findcurrentspace.py
@@ -1,33 +1,30 @@
-def createquery(y,z,currentspace):
-    flag1=False
-    for v in currentspace.values():
-        if len(v)!=0:
-                for i in v:
-                    if type(i) is dict:
-                        if y in i.keys():
-                            flag1=False
-                        elif z in i.keys():
-                            currentspace=i
-                            createquery(y,z,i)
-                        else:
-                            flag1=True
-                    else:
-                        continue
-        else:
-            v.append({y:[]})
-    if flag1==True:
-            v.append({y:[]})
-def findcurrentspace(z,currentspace):
-    if z in currentspace:
-        print('pidor')
-    else:
-        findcurrentspace(z,
-data='create level1 global\ncreate level2 level1\ncreate level3 level2\ncreate level4 level3\n'
-xdata=data.splitlines()
-namespaces={'global':[]}
+def ffindcurrentspace(z,currentspace):
+    result=None
+    def findcurrentspace(z,currentspace):
+        nonlocal result
+        for v in currentspace:
+            if v==z:
+                currentspace=v
+            else:
+                for v in currentspace.values():
+                    for i in v:
+                        if type(i) is dict:
+                            if z in i.keys():
+                                result=i
+                            else:
+                                currentspace=i
+                                findcurrentspace(z,currentspace)
+    findcurrentspace(z,currentspace)
+    return result
+
+#namespaces={'global':[]}
+#namespaces={'global':['a','b']}
+#namespaces={'global':['a','b',{'level1':[]}]}
+#namespaces={'global':['a','b',{'level1':[]},'c']}
+#namespaces={'global':['a','b',{'level2':[]}]}
+#namespaces={'global':['a','b',{'level2':[]},'c']}
+#namespaces={'global':['a','b',{'level2':[]},'c',{'level3':[]}]}
+namespaces={'global':['a','b',{'level1':[{'level4':[]}]},'c',{'level2':[{'level5':[]}]},'d',{'level3':[{'level6':[]}]}]}
 currentspace=namespaces
-for i in xdata:
-    x,y,z=i.split()
-    if x=='create':
-        findcurrentspace(z,currentspace)
-        createquery(y,z,currentspace)
+y,z='level7','level6'
+ffindcurrentspace(z,currentspace)
