723013159984376ee689fb55e043d7dfd992364c
pzelenin92 <pzelenin92@gmail.com>
2020-06-04 16:11:51 +0300

    1.6.8 and 1.6.9

---
+++ stepik#512/1.6/1.6.9/1.6.9.py
@@ -0,0 +1,11 @@
+import time
+
+class Loggable:
+    def log(self, msg):
+        print(str(time.ctime()) + ": " + str(msg))
+class LoggableList(list,Loggable):
+    def append(self,x):
+        super(LoggableList,self).append(x)
+        self.log(x)
+x=LoggableList()
+x.append(3)
