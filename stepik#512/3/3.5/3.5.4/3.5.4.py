834b688ca5bede8a78566a6bb65abaed0914d2a3
pzelenin92 <pzelenin92@gmail.com>
2020-10-12 18:51:33 +0300

    done 3.5.4

--- stepik#512/3/3.5/3.5.4/3.5.4.py
+++ stepik#512/3/3.5/3.5.4/3.5.4.py
@@ -1,8 +1,27 @@
-# import json
-# #написать ввод json формата
-# si=[{"name": "A", "parents": []}, {"name": "B", "parents": ["A", "C"]}, {"name": "C", "parents": ["A"]}]
-# datadumps=json.dumps(si, indent=4)
-# dataloads=json.loads(datadumps)
-# print(dataloads)
-class B: pass
-print(issubclass(B, B))
+import json
+
+#test input
+#inp_json='[{"name": "A", "parents": []}, {"name": "B", "parents": ["A", "C"]}, {"name": "C", "parents": ["A"]}]'
+inp_json = '[{"name": "G", "parents" :["F"]}, { "name": "A", "parents": []},{"name" :"B", "parents": ["A"]},{"name": "C", "parents": ["A"]},{"name": "D", "parents": ["B", "C"]},{"name": "E", "parents": ["D"]},{"name": "F", "parents": ["D"]},{"name": "X", "parents": []},{"name": "Y", "parents": ["X", "A"]}, {"name": "Z", "parents": ["X"]},{"name": "V", "parents": ["Z", "Y"]},{"name": "W", "parents": ["V"]}]'
+
+datajsontopy=json.loads(inp_json)
+d={}
+
+def foo(x,y):
+    if y not in d:
+        d[y]=0
+    if x not in s: # проверка на наличие в сете для отсеивания лишних прибавлений количества предков
+        d[x]+=1
+        s.add(x)
+    for k in datajsontopy:
+        if y in k["parents"]:
+            if k["name"] not in s: # проверка на наличие в сете для отсеивания лишних прибавлений количества предков
+                d[x]+=1
+                s.add(k["name"])
+                foo(x,k["name"])
+
+for i in datajsontopy:
+    s=set()
+    foo(i["name"],i["name"])
+for k,v in sorted(zip(d.keys(),d.values())):
+    print(k,":",v)
