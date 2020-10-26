e36556693bc746ba0ac9b829277bccce7c244dd2
pzelenin92 <pzelenin92@gmail.com>
2020-10-26 21:59:35 +0300

    some changes itn forwork.py

--- myscripts/forwork/forwork.py
+++ myscripts/forwork/forwork.py
@@ -1,20 +1,35 @@
 import csv
+import os
+
 workers = [
-    ["Name","Counter","Holidays"],
     ["Maxim",0],
     ["Pavel",0],
     ["Ivan",1,"4е ноября"],
     ["Andrei",1,"4е ноября"]
 ]
+header = [
+    ["Name","Counter","Holidays"]
+]
+data = []
+x = "test1.csv"
 
-with open ("test1.csv",'r+',newline='') as testfile:
-    reader=csv.reader(testfile)
-    writer=csv.writer(testfile)
-    try:
-        next(reader)
-    except StopIteration:
-        print("file is empy")
-        writer.writerow(workers[0])
+if x in os.listdir():
+    #считываем файл
+    with open ("test1.csv",'r',newline='') as testfile:
+        reader=csv.reader(testfile)
+    #собираем дату в лист
+        l=[]
+        for row in reader:
+            l.append(row)
+        print(l)
+
+
+else:
+    #создаем файл и запишем туда обработанную дату
+    with open ("test1.csv",'w',newline='') as testfile:
+        writer=csv.writer(testfile)
+        #запишем дату
+        writer.writerows(workers)
 
 
 
