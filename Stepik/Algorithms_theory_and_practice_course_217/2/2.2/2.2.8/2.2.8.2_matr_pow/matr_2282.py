6a30e2545c187da4fb9e198609ac6c1744952db7
pzelenin92 <pzelenin92@gmail.com>
2020-12-04 18:35:45 +0300

    fixed some problems and pylint warnings

--- Stepik/Algorithms_theory_and_practice_course_217/2/2.2/2.2.8/2.2.8.2_matr_pow/matr_2282.py
+++ Stepik/Algorithms_theory_and_practice_course_217/2/2.2/2.2.8/2.2.8.2_matr_pow/matr_2282.py
@@ -1,9 +1,10 @@
-"""This script computes Fibonacci Number mod m"""
+"""This script computes n-th Fibonacci Number mod m"""
 
 
-def fast_pow_matr(n):
+def fib_mod(n, m):
     """This function makes fast exponentiation of the given matrix in order to find
-    n-th fibonacci number.There is used fast exponentiation alg"""
+    n-th fibonacci number.At the same time it computes fib(n)mod m instead of
+    calculating fibonacci numbers itself.There is used fast exponentiation alg"""
     pa, pb, pc, pd = 1, 0, 0, 1
     a, b, c, d = 1, 1, 1, 0
 
@@ -20,23 +21,23 @@ def fast_pow_matr(n):
             pbt = a * pb + b * pd
             pct = c * pa + d * pc
             pdt = c * pb + d * pd
-            pa, pb, pc, pd = pat, pbt, pct, pdt
+            pa, pb, pc, pd = pat % m, pbt % m, pct % m, pdt % m
             n -= 1
 
         at = a * a + b * c
         bt = a * b + b * d
         ct = c * a + d * c
         dt = c * b + d * d
-        a, b, c, d = at, bt, ct, dt
+        a, b, c, d = at % m, bt % m, ct % m, dt % m
         n = n // 2
 
     return pb
 
 
 def main():
-    """Simple main function that takes n variable as power"""
-    n = int(input())
-    print(fast_pow_matr(n))
+    """Simple main function that takes n,m variables"""
+    n, m = map(int, input().split())
+    print(fib_mod(n, m))
 
 
 if __name__ == "__main__":
--- Stepik/Algorithms_theory_and_practice_course_217/2/2.2/2.2.8/2.2.8.3_pre_algs/fast_power.py
+++ Stepik/Algorithms_theory_and_practice_course_217/2/2.2/2.2.8/2.2.8.3_pre_algs/fast_power.py
@@ -1,12 +1,16 @@
+"""Simple algorithm of fast powering"""
 
-def fast_pow(x,n):
-    p=1
-    while n!=0:
-        if n%2 == 1:
-            p=p*x
-            n-=1
-        x=x**2
-        n=n//2
+
+def fast_pow(x, n):
+    """x multiplies n times"""
+    p = 1
+    while n != 0:
+        if n % 2 == 1:
+            p = p * x
+            n -= 1
+        x = x ** 2
+        n = n // 2
     return p
 
-print(fast_pow(2,1))
+
+print(fast_pow(3, 5))
