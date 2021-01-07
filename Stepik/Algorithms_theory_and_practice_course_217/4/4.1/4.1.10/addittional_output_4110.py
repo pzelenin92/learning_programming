bdd9cb809fbbe9d63d18c6605e64325f3a254459
pzelenin92 <pzelenin92@gmail.com>
2021-01-07 16:43:45 +0300

    4.1.10 done

---
+++ Stepik/Algorithms_theory_and_practice_course_217/4/4.1/4.1.10/addittional_output_4110.py
@@ -0,0 +1,41 @@
+"""This script solves the problem of continuous rucksack with greedy algorithm"""
+
+
+def max_rucksack_cap(capacity, items):
+    """function that puts optimal quantity of items in order to maximize its overall price"""
+    print(items)
+
+    items.sort(key=lambda x: x[0] / x[1], reverse=True)
+    print(items)
+
+    rucksack, maxprice = [], 0
+
+    for item in items:
+        if item[1] < capacity:
+            rucksack.append(item[1])
+            maxprice += item[0]
+            capacity -= item[1]
+        else:
+            rucksack.append(capacity)
+            maxprice += (item[0] / item[1]) * capacity
+            break
+
+    print(rucksack)
+    print(format(maxprice, '.3f'))
+
+
+def main():
+    """main function"""
+    n, capacity = map(int, input().split())
+    items = []
+    while n > 0:
+        input_item = [int(i) for i in input().split()]
+        if 0 not in input_item:
+            items.append(input_item)
+        n -= 1
+
+    max_rucksack_cap(capacity, items)
+
+
+if __name__ == '__main__':
+    main()
